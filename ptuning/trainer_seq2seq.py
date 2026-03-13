7c9f18ef3d324eda792a4a6997ba1af4e7938f16
肖保强 <xiaobq@shuhaisc.com>
2026-03-13 16:59:03 +0800

    删除transformers.deepspeed

--- ptuning/trainer_seq2seq.py
+++ ptuning/trainer_seq2seq.py
@@ -13,12 +13,9 @@
 # limitations under the License.
 
 from typing import Any, Dict, List, Optional, Tuple, Union
-
 import torch
 from torch import nn
 from torch.utils.data import Dataset
-
-from transformers.deepspeed import is_deepspeed_zero3_enabled
 from trainer import PrefixTrainer
 from transformers.trainer_utils import PredictionOutput
 from transformers.utils import logging
@@ -178,7 +175,7 @@ class Seq2SeqTrainer(PrefixTrainer):
         gen_kwargs["num_beams"] = (
             gen_kwargs["num_beams"] if gen_kwargs.get("num_beams") is not None else self.model.config.num_beams
         )
-        default_synced_gpus = True if is_deepspeed_zero3_enabled() else False
+        default_synced_gpus = False
         gen_kwargs["synced_gpus"] = (
             gen_kwargs["synced_gpus"] if gen_kwargs.get("synced_gpus") is not None else default_synced_gpus
         )
